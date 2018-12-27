170ffc7ac2c5eff412abef9fedfea8e118fe59a6
pablowbk <pablo.weisbek@gmail.com>
2018-12-27 10:35:34 -0300

    SPECIES done! sttill working on films array

--- swapiapp/src/CardComponent.js
+++ swapiapp/src/CardComponent.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 
-const CardComponent = ({ name, species, films }) => {
 
-  return (
-    <div className='Card'>
-      <h3>{ name }</h3>
-      <h4 style={{fontStyle: 'italic'}}>The species.name value should be shown below... not the url</h4>
-      <h4>{ species }</h4>
-      <div>Featured in:
-        <p style={{fontStyle: 'italic'}}>(movie titles should be show in the list below, not the urls...)</p>
-          <ul>
-            {films.map((film, i) => (
-              <li key={i}>
-                { film }
-              </li>
-            ))}
-          </ul>
+
+class CardComponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      species: ['Fetching species...'],
+      films: 'Loading films...'
+    }
+  }
+
+  componentDidMount() {
+    fetch(this.props.species[0])
+      .then(resp => resp.json())
+      .then(json => this.setState({species: json.name}))
+
+    let filmsArray = [];
+    this.props.films.map(film => {
+      return (
+        fetch(film)
+        .then(resp => resp.json())
+        .then(json => filmsArray.push(`Episode ${json.episode_id}: ${json.title}`))
+      );
+    })
+    this.setState({films:filmsArray})
+    console.log('array', filmsArray);
+    console.log('state', this.state.films);
+
+  }
+
+
+
+
+  render() {
+    const { name } = this.props;
+
+    return (
+      <div className='Card'>
+        <h3>{ name }</h3>
+        {/* <h4 style={{fontStyle: 'italic'}}>The species.name value should be shown below... not the url</h4> */}
+        <h4>{ this.state.species }</h4>
+        <div>Featured in:
+          {/* <p style={{fontStyle: 'italic'}}>(movie titles should be show in the list below, not the urls...)</p> */}
+            <ul>
+              { this.state.films.map((film, i) => (
+                <li key={i}>
+                  { film }
+                </li>
+              ))}
+            </ul>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 export default CardComponent;
